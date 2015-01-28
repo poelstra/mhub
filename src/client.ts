5f78d991d7e8fc1cde6d0aa8507659f868f1c642
Martin Poelstra <martin@beryllium.net>
2015-01-28 22:57:55 +0100

    Implement simple CLI for publishing/subscribing.

--- src/client.ts
+++ src/client.ts
@@ -1,1 +1,123 @@
-// TODO...
+/**
+ * Commandline tool for sending/receiving mserver messages.
+ */
+
+/// <reference path="../typings/tsd.d.ts" />
+
+"use strict";
+
+import yargs = require("yargs");
+import ws = require("ws");
+
+var usage = [
+	"Listen mode: $0 -s <host>[:<port>] -n <nodename> -l",
+	"Post mode: $0 -s <host>[:<port>] -n <nodename> -t <topic> [-d <json_data>] [-h <json_headers>]",
+].join("\n");
+
+function die(...args: any[]): void {
+	console.log.apply(this, args);
+	process.exit(1);
+}
+
+var args = yargs
+	.usage(usage)
+	.help("help")
+	.option("s", {
+		type: "string",
+		alias: "socket",
+		description: "WebSocket to connect to",
+		default: "localhost:13900"
+	})
+	.option("n", {
+		type: "string",
+		alias: "node",
+		description: "Node to subscribe/publish to",
+		required: true
+	})
+	.option("l", {
+		type: "boolean",
+		alias: "listen",
+		description: "Select listen mode"
+	})
+	.option("t", {
+		type: "string",
+		alias: "topic",
+		description: "Message topic"
+	})
+	.option("d", {
+		type: "string",
+		alias: "data",
+		description: "Optional message data as JSON object, e.g. '\"a string\"' or '{ \"foo\": \"bar\" }'"
+	})
+	.option("h", {
+		type: "string",
+		alias: "headers",
+		description: "Optional message headers as JSON object, e.g. '{ \"my-header\": \"foo\" }'"
+	});
+
+function listenMode(): void {
+	var argv = args.argv;
+	var socket = new ws("ws://" + argv.socket);
+	socket.on("open", (): void => {
+		socket.send(JSON.stringify({
+			type: "subscribe",
+			node: argv.node
+		}));
+		socket.on("message", (data: string): void => {
+			console.log(data);
+		});
+	});
+	socket.on("error", (e: Error): void => {
+		die("Socket error:", e);
+	});
+}
+
+function postMode(): void {
+	var argv = args
+		.require("topic", true)
+		.argv;
+
+	var data: any;
+	try {
+		data = argv.data && JSON.parse(argv.data);
+	} catch (e) {
+		console.log("Error parsing message data as JSON: " + e.message);
+		die(
+			"Hint: if you're passing a string, make sure to put double-quotes around it, " +
+			"and escape these quotes for your shell with single-quotes, e.g.: '\"my string\"'"
+		);
+	}
+
+	var headers: any;
+	try {
+		headers = argv.headers && JSON.parse(argv.headers);
+	} catch (e) {
+		die("Error parsing message headers as JSON: " + e.message);
+	}
+
+	var socket = new ws("ws://" + argv.socket);
+	socket.on("open", (): void => {
+		socket.send(JSON.stringify({
+			type: "publish",
+			node: argv.node,
+			topic: argv.topic,
+			data: data,
+			headers: headers
+		}), (): void => {
+			socket.close();
+		});
+	});
+	socket.on("error", (e: Error): void => {
+		die("Socket error:", e);
+	});
+}
+
+var argv = args.argv;
+
+if (argv.listen) {
+	listenMode();
+} else if (argv.topic) {
+	postMode();
+} else {
+	die("Either -l or -t is required, see --help for more info.");
+}
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -3,3 +3,4 @@
 /// <reference path="ws/ws.d.ts" />
 /// <reference path="basic-auth/basic-auth.d.ts" />
 /// <reference path="minimatch/minimatch.d.ts" />
+/// <reference path="yargs/yargs.d.ts" />
