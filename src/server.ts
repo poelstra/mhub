f396c4367dd0a0a276591d76b891e50f4646e148
Martin Poelstra <martin@beryllium.net>
2015-01-29 21:47:00 +0100

    server: Remove basic-auth for now, didn't work for WebSockets anyway.

--- src/server.ts
+++ src/server.ts
@@ -4,32 +4,11 @@
 
 import express = require("express");
 import http = require("http");
-import basicAuth = require("basic-auth");
 import pubsub = require("./pubsub");
 import SocketHub = require("./SocketHub");
 
-function auth(req: express.Request, res: express.Response, next: Function): express.Response {
-	function unauthorized(): express.Response {
-		res.set("WWW-Authenticate", "Basic realm=Authorization Required");
-		return res.send(401);
-	};
-
-	var user = basicAuth(req);
-
-	if (!user || !user.name || !user.pass) {
-		return unauthorized();
-	};
-
-	if (user.name === "fll" && user.pass === "supercool") {
-		return next();
-	} else {
-		return unauthorized();
-	};
-};
-
 var app = express();
 
-app.use(auth);
 //app.use("/", express.static(__dirname + "/static"));
 
 var PORT = process.env.PORT || 13900;
