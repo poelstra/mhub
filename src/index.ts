53c176f5fa1583224be622d274babc1cf41856b9
Rikkert Koppes <rikkert@rikkertkoppes.com>
2018-02-26 14:59:50 +0100

    export MServer

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@
 import MClient from "./nodeclient";
 export default MClient;
 export * from "./nodeclient";
+export { MServer } from './nodeserver';
 
 export { default as Message } from "./message";
